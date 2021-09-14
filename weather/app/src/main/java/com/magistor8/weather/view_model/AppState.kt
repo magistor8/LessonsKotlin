caea98f092c6c0b853060567183f380cf47c7fbb
Alexey <a.gorbunov88@yandex.ru>
2021-09-14 15:51:50 +0300

    ДЗ4

--- weather/app/src/main/java/com/magistor8/weather/view_model/AppState.kt
+++ weather/app/src/main/java/com/magistor8/weather/view_model/AppState.kt
@@ -6,4 +6,5 @@ sealed class AppState {
     data class Success(val weatherData: List<Weather>) : AppState()
     data class Error(val error: Throwable) : AppState()
     object Loading : AppState()
+    object Null : AppState()
 }
